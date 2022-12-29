5893c4bd026665efbef4444809244502aec2ff03
Blue-Shrimp <wkdtmd1202@naver.com>
2022-12-29 10:37:36 +0900

    feat: 프로젝트 구조 설정 및 babel 별칭 경로 설정

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
 import Config from 'react-native-config'
+
+global.appConfig = {
+  envName: Config.ENV,
+  buildVersion: Config.BUILD_VERSION,
+  appVersion: Config.APP_VERSION,
+  appName: Config.APP_NAME,
+  appId: Config.APP_ID,
+}
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ENV : {Config.ENV}</Text>
-      <Text>Build Version : {Config.BUILD_VERSION}</Text>
-      <Text>App Version : {Config.APP_VERSION}</Text>
-      <Text>APP Name: {Config.APP_NAME}</Text>
-      <Text>APP ID : {Config.APP_ID}</Text>
+      <Text>ENV : {appConfig.envName}</Text>
+      <Text>Build Version : {appConfig.buildVersion}</Text>
+      <Text>App Version : {appConfig.appVersion}</Text>
+      <Text>APP Name: {appConfig.appName}</Text>
+      <Text>APP ID : {appConfig.appId}</Text>
+      <Image source={require('@images/marker.png')}></Image>
     </SafeAreaView>
   )
 }
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        global: ['./'],
+        extensions: ['.js', '.json'],
+        alias: {
+          '@root': './',
+          '@common': './src/common',
+          '@stores': './src/store',
+          '@network': './src/network',
+          '@screens': './src/screen',
+          '@extensions': './src/extension',
+          '@components': './src/components',
+          '@images': './assets/images',
+        },
+      },
+    ],
+  ],
+}
